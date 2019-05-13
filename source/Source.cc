a41f81f5f1902b25719946dab938cb43b47e0103
Matej Roguljic <matej.roguljic@gmail.com>
2019-05-13 13:47:41 +0200

    minor bug fix

--- source/Source.cc
+++ source/Source.cc
@@ -34,7 +34,7 @@ int main(int argc,char** argv)
   //
 #ifdef G4MULTITHREADED
   //G4MTRunManager* runManager = new G4MTRunManager;
-  G4MTRunManager* runManager = new G4RunManager;
+  G4RunManager* runManager = new G4RunManager;
 #else
   G4RunManager* runManager = new G4RunManager;
 #endif
